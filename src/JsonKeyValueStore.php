557cc0e687fe74865c4301e1019532a7ed9c80ef
peter279k <peter279k@gmail.com>
2018-12-08 18:43:51 +0800

    Add type hint with PHPDoc comment annotation style

--- src/JsonKeyValueStore.php
+++ src/JsonKeyValueStore.php
@@ -4,21 +4,43 @@ namespace DivineOmega\JsonKeyValueStore;
 
 class JsonKeyValueStore
 {
+    /** @var string */
     private $file;
+
+    /** @var mixed */
     private $content;
 
-    public function __construct($file)
+    /**
+     * Constructor.
+     *
+     * @param string $file
+     *
+     * @return void
+     */
+    public function __construct(string $file)
     {
         $this->file = $file;
         $this->load();
     }
 
+    /**
+     * Create empty store.
+     *
+     * @return void
+     */
     private function createEmptyStore()
     {
         $this->content = new \stdClass();
         $this->save();
     }
 
+    /**
+     * Load contents from store.
+     *
+     * @throws Exception
+     *
+     * @return void
+     */
     private function load()
     {
         if (!file_exists($this->file)) {
@@ -43,6 +65,13 @@ class JsonKeyValueStore
         }
     }
 
+    /**
+     * Save contents from store.
+     *
+     * @throws Exception
+     *
+     * @return void
+     */
     private function save()
     {
         $encodedContent = gzencode(json_encode($this->content));
@@ -64,17 +93,37 @@ class JsonKeyValueStore
         }
     }
 
+    /**
+     * Get file path.
+     *
+     * @return string
+     */
     public function getFile()
     {
         return $this->file;
     }
 
+    /**
+     * Set content with key and value.
+     *
+     * @param mixed $key
+     * @param mixed $value
+     *
+     * @return void
+     */
     public function set($key, $value)
     {
         $this->content->$key = $value;
         $this->save();
     }
 
+    /**
+     * Get value with specific key.
+     *
+     * @param mixed $key
+     *
+     * @return mixed
+     */
     public function get($key)
     {
         if (!isset($this->content->$key)) {
@@ -84,6 +133,11 @@ class JsonKeyValueStore
         return $this->content->$key;
     }
 
+    /**
+     * Delete value with specific key.
+     *
+     * @return null
+     */
     public function delete($key)
     {
         unset($this->content->$key);
